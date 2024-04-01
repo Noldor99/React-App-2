ecc59f560f2bea542f72a29ae14e95cd514342c4
Noldor99 <qwerr19942017@gmail.com>
2024-04-01 20:27:15 +0300

    new link

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_PUBLIC_API_URLL ?? "https://taskboardother.onrender.com/api",
+  baseURL: import.meta.env.VITE_PUBLIC_API_URLL ?? "https://react-app-2-2hys.onrender.com/api",
   withCredentials: true,
 })
 
